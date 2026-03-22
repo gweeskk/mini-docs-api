9a04a07ee7b71104e361d6335739e75bdc02ec7d
Alena <serbinovitch.alyona@yandex.ru>
2026-03-22 18:05:46 +0300

    index.html

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
 const db = require("./db");
+const cors = require("cors");
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.url}`);
